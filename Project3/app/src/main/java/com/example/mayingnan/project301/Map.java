006238a504c1976dcd09575855a23e9eec3ce80f
yueMaHello <ym5@ualberta.ca>
2018-02-25 22:49:26 -0700

    add some unsure functions in Map

--- Project3/app/src/main/java/com/example/mayingnan/project301/Map.java
+++ Project3/app/src/main/java/com/example/mayingnan/project301/Map.java
@@ -1,9 +1,16 @@
 package com.example.mayingnan.project301;
 
+import android.webkit.GeolocationPermissions;
+
 /**
  * Created by User on 2018/2/25.
  */
 
 public class Map {
+    public void showAllAvailableTasks(){
 
+    }
+    public void showDistance(){
+        //when the task is processing, the map should show process's location and requeter's location
+    }
 }
