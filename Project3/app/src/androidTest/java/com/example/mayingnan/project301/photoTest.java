ad3449c05bfa1fc0625224d88e02f07e46db8599
XingyuanYang <xy3@ualberta.ca>
2018-02-26 14:26:20 -0700

    .

--- Project3/app/src/androidTest/java/com/example/mayingnan/project301/photoTest.java
+++ Project3/app/src/androidTest/java/com/example/mayingnan/project301/photoTest.java
@@ -1,8 +1,13 @@
 package com.example.mayingnan.project301;
 
+import android.media.Image;
+import android.media.Image.Plane;
+import android.provider.ContactsContract;
 import android.test.ActivityInstrumentationTestCase2;
 
 import com.example.mayingnan.project301.allUserActivity.LogInActivity;
+
+import java.nio.ByteBuffer;
 
 /**
  * Created by julianstys on 2018-02-25.
@@ -13,8 +18,10 @@ public class photoTest extends ActivityInstrumentationTestCase2 {
     public photoTest() {
         super(LogInActivity.class);
     }
+
     public void addPhotoTest(){
-         assertTrue(true);
+
+        assertTrue(true);
     }
     public void updatePhotoTest(){
         assertTrue(true);
