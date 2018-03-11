e4daf43b8c77dd5ffddd275da479d05b0119de37
yueMaHello <ym5@ualberta.ca>
2018-03-10 19:33:16 -0700

    merge issue fixed

--- app/src/main/java/com/example/mayingnan/project301/allUserActivity/SignUpActivity.java
+++ app/src/main/java/com/example/mayingnan/project301/allUserActivity/SignUpActivity.java
@@ -1,11 +1,9 @@
-
-
 package com.example.mayingnan.project301.allUserActivity;
 
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
