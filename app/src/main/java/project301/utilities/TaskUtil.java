c3183b53f8fdf1424583b82f94c9e3c7d51abd34
Yuqi Zhang <yuqi8@ualberta.ca>
2018-04-07 15:07:03 -0600

    validate phone format

--- app/src/main/java/project301/utilities/TaskUtil.java
+++ app/src/main/java/project301/utilities/TaskUtil.java
@@ -121,6 +121,8 @@ public class TaskUtil {
     /**
      * constant of email format
      * https://stackoverflow.com/questions/8204680/java-regex-email
+     * @param nothing
+     * @return nothing
      */
     public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
@@ -128,9 +130,31 @@ public class TaskUtil {
     /**
      * check email format
      * https://stackoverflow.com/questions/8204680/java-regex-email
+     * @param emailStr user input emali format
+     * @return is_correct_format
      */
-    public static boolean validate(String emailStr) {
+    public static boolean validate_email(String emailStr) {
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+    }
+
+    /**
+     * constant of email format
+     * https://stackoverflow.com/questions/8204680/java-regex-email
+     * @param nothing
+     * @return nothing
+     */
+    public static final Pattern VALID_PHONE_REGEX =
+            Pattern.compile("^[0-9]", Pattern.CASE_INSENSITIVE);
+
+    /**
+     * check email format
+     * https://stackoverflow.com/questions/8204680/java-regex-email
+     * @param phoneStr user input emali format
+     * @return is_correct_format
+     */
+    public static boolean validate_phone(String phoneStr) {
+        Matcher matcher = VALID_PHONE_REGEX .matcher(phoneStr);
         return matcher.find();
     }
 
