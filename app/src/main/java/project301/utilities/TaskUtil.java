d83d616315e39b64907cffcea1c1cc87793f7b44
Yuqi Zhang <yuqi8@ualberta.ca>
2018-04-07 20:03:56 -0600

    phone format checked

--- app/src/main/java/project301/utilities/TaskUtil.java
+++ app/src/main/java/project301/utilities/TaskUtil.java
@@ -145,7 +145,7 @@ public class TaskUtil {
      * @return nothing
      */
     public static final Pattern VALID_PHONE_REGEX =
-            Pattern.compile("^[0-9]", Pattern.CASE_INSENSITIVE);
+            Pattern.compile("^[0-9]{10,10}", Pattern.CASE_INSENSITIVE);
 
     /**
      * check email format
